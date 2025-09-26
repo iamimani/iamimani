e97fb01ebb6c24f6aae177d7313d66397b867a9d
iamimani <gitaufaith70@gmail.com>
2025-09-26 07:29:17 -0700

    Update faithGitau.js

--- faithGitau.js
+++ faithGitau.js
@@ -1,16 +1,22 @@
-function isEven(num) {
-  // Use modulo operator to check divisibility by 2
-  if (num % 2 === 0) {
-     return true;
-  } else {
-    return false;
-  }
+function reverseString(str) {
+  // 1. Split the string into an array of characters.
+  const charArray = str.split('');
+  
+  // 2. Reverse the array of characters.
+  const reversedArray = charArray.reverse();
+  
+  // 3. Join the characters back into a string.
+  const reversedString = reversedArray.join('');
+  
+  // 4. Return the reversed string.
+  return reversedString;
+  
+  // Alternative one-line solution:
+  // return str.split('').reverse().join('');
 }
 
-//Test cases
-console.log(isEven(10));  //
- output: true
-console.log(isEven(15)); //
- output: false
-console.log(isEven(-8)); //
- ouput: true
+// Test cases
+console.log(reverseString("hello")); // Expected output: "olleh"
+console.log(reverseString("12345")); // Expected output: "54321"
+console.log(reverseString("")); // Expected output: "" (empty string remains empty)
+console.log(reverseString("JavaScript")); // Expected output: "tpircSavaJ"
